460520b8b72201ca2687b54f1c0b7826e5b9c93d
1naresh <mergu345@gmail.com>
2021-04-28 09:38:25 +0530

    Add files via upload
    - revision basics

--- index.js
+++ index.js
@@ -3,19 +3,27 @@
 
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Lcmethods from "./basics/lc-methods1";
+import Comp1 from './basics/comp1';
+import Comp2 from './basics/comp2';
 
 function App(){
-    let result = useState(true)
-    let show = result[0]
-    let setShow = result[1]
-    console.log("parent is rendering")
+    let result = useState("mark")
+    let sname = result[0]
+    let setsName = result[1]
+    const change = () =>{
+        setsName("steve")
+    }
     return(
         <div>
-            parent
-            <button onClick={()=> setShow(false) } >hide</button>
-            { show && <Lcmethods />}
-            
+            <div style={{float:"left",width:"40%",height:"200px",border:"1px solid black"}} >
+                <Comp1 prop1={sname} />
+            </div>
+            <div style={{float:"left",width:"40%",height:"200px",border:"1px solid black"}} >
+                <Comp2 prop1={sname} />
+            </div>
+            {/* <button onClick={change} >
+                change
+            </button> */}
         </div>
     )
 }
